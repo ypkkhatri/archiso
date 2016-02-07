f8697bd41d8999952721267980e9962534f9349d
Yougeshwar <Yougeshwar@YOUGESH>
2016-02-08 02:11:00 +0500

    Signed-off-by: Yougeshwar <Yougeshwar@YOUGESH>

--- MechanicsTool/src/com/yog/fw/core/dao/DaoRepository.java
+++ MechanicsTool/src/com/yog/fw/core/dao/DaoRepository.java
@@ -1,9 +1,9 @@
 package com.yog.fw.core.dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.yog.mt.models.BaseModel;
 
-public interface DaoRepository extends JpaRepository<BaseModel, Long> {
-
-}
+//public interface DaoRepository extends JpaRepository<BaseModel, Long> {
+//
+//}
